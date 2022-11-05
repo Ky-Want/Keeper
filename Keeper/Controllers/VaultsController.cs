71725e62e05c6e04cbbea21e1150c827de369d54
Lumine3449 <>
2022-11-05 14:25:47 -0600

    minor edit

--- Keeper/Controllers/VaultsController.cs
+++ Keeper/Controllers/VaultsController.cs
@@ -5,8 +5,5 @@ namespace Keeper.Controllers;
 [Route("api/[controller]")]
 public class VaultsController : ControllerBase
 {
-  private readonly Auth0Provider _a0;
-  private readonly VaultsService _vs;
-
 
 }
