5570e7b35538ba1b69d5d21a460b8d90314799f5
Lumine3449 <>
2022-11-05 14:33:15 -0600

    vaults controller and service initialized

--- Keeper/Controllers/VaultsController.cs
+++ Keeper/Controllers/VaultsController.cs
@@ -5,5 +5,14 @@ namespace Keeper.Controllers;
 [Route("api/[controller]")]
 public class VaultsController : ControllerBase
 {
+  private readonly Auth0Provider _a0;
+  private readonly VaultsService _vs;
+  private readonly KeepsService _ks;
 
+  public VaultsController(Auth0Provider a0, VaultsService vs, KeepsService ks)
+  {
+    _a0 = a0;
+    _ks = ks;
+    _vs = vs;
+  }
 }
--- Keeper/Services/VaultsService.cs
+++ Keeper/Services/VaultsService.cs
@@ -4,4 +4,11 @@ namespace Keeper.Services;
 public class VaultsService
 {
   private readonly VaultsRepository _vr;
+  private readonly VaultKeepsRepository _vkRepo;
+
+  public VaultsService(VaultsRepository vr, VaultKeepsRepository vkRepo)
+  {
+    _vr = vr;
+    _vkRepo = vkRepo;
+  }
 }
